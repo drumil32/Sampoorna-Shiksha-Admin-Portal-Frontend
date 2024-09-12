b1d9570ad99dec9fdb3de991fecbb8f5caec1b99
drumil32 <akheniad@gmail.com>
2024-09-12 23:11:32 +0530

    fix

--- src/Components/OrderHistoryTable.tsx
+++ src/Components/OrderHistoryTable.tsx
@@ -1,31 +1,28 @@
-import React ,{useState , useEffect} from 'react'
-import axiosInstance from '../utils/axiosInstance';
-import { GET_ALL_VENDOR_ORDER } from '../utils/restEndPoints';
-import { useDispatch } from "react-redux";
-import { setLoading , setError } from '../redux/slices/statusSlice';
-import { toast } from "react-toastify";
-import { Action } from '../types/error';
+import React, { useEffect, useState } from 'react';
 import { FaFilter } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 
 
-import { VendorOrder , VendorOrderType ,VendorOrderStatus } from '../types/VendorOrder';
+import { VendorOrder, VendorOrderType, VendorOrderStatus } from '../types/VendorOrder';
 
-const OrderHistoryTable :React.FC = () => {
+const OrderHistoryTable: React.FC<{ orders: VendorOrder[] }> = ({ orders }) => {
 
-  const dispatch = useDispatch();
-  const [orders, setOrders] = useState<VendorOrder[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [orderType, setOrderType] = useState<VendorOrderType | undefined>(undefined);
   const [orderStatus, setOrderStatus] = useState<VendorOrderStatus | undefined>(undefined);
-  const [filterOrders, setFilterOrders] = useState<VendorOrder[]>([]);
+  const [filterOrders, setFilterOrders] = useState<VendorOrder[]>(orders);
+
+  useEffect(() => {
+    setFilterOrders(orders);
+  }, [orders]);
+
   const navigate = useNavigate();
 
-    const matchOrderBrandOrSubBrand = (orderName: string) => {
+  const matchOrderBrandOrSubBrand = (orderName: string) => {
     return '' == searchQuery || orderName.toLowerCase().includes(searchQuery.toLowerCase());
   };
 
-    const handleSearch = () => {
+  const handleSearch = () => {
     setFilterOrders(
       orders.filter((order) =>
         (order.type == orderType || orderType == undefined) &&
@@ -35,146 +32,118 @@ const OrderHistoryTable :React.FC = () => {
     );
   }
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      dispatch(setLoading(true));
-      const response = await axiosInstance.get(GET_ALL_VENDOR_ORDER);
-      setOrders(response.data);
-    //   setFilterOrders(response.data);
-    } catch (error: any) {
-      if (error.response) {
-        dispatch(
-          setError({
-            statusCode: error.response.status,
-            message: error.response.data.error,
-            action: Action.VENDOR_ORDER_HISTORY,
-          })
-        );
-      } else {
-        toast.error("Server is Down.");
-      }
-    } finally {
-      dispatch(setLoading(false));
-    }
-  };
-
-//   fetchData();
-},[]);
-
   return (
     <div>
-    <div className='filters flex sm:flex-row flex-col mt-3 sm:max-w-5xl m-auto gap-2 w-[90%] justify-between'>
-          <div className='pl-2 flex gap-2 '>
-            <input
-              type='text'
-              placeholder='Brand Or SubBrand...'
-              className='text-xs  outline-none border p-2 w-[300px] rounded-sm'
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            <select
-              className='border rounded-sm shadow-sm block  text-[12px] outline-none p-1'
-              onChange={(e) =>
-                setOrderType(
-                  e.target.value === "All"
-                    ? undefined
-                    : VendorOrderType[
-                        e.target.value as keyof typeof VendorOrderType
-                      ]
-                )
-              }
-            >
-              {["All", ...Object.keys(VendorOrderType)].map((orderType) => (
-                <option value={orderType}>{orderType}</option>
-              ))}
-            </select>
-
-            <select
-              className='border rounded-md shadow-sm block text-[12px] outline-none p-1'
-              onChange={(e) =>
-                setOrderStatus(
-                  e.target.value === "All"
-                    ? undefined
-                    : VendorOrderStatus[
-                        e.target.value as keyof typeof VendorOrderStatus
-                      ]
-                )
-              }
-            >
-              {["All", ...Object.keys(VendorOrderStatus)].map((status) => (
-                <option value={status} className=''>
-                  {status}
-                </option>
-              ))}
-            </select>
-
-            <select
-              name='from'
-              className='border rounded-md shadow-md block w-full p-3 text-xs outline-none'
-              // value={from}
-            >
-              <option value='' disabled selected>From</option>
-              <option value='vendor'>vendor</option>
-              <option value='ngo'>ngo</option>
-            </select>
-
-            <select
-              name='to'
-              className='border rounded-md shadow-md block w-full p-3 text-xs outline-none'
-              // value={from}
-            >
-              <option value='' disabled selected>To</option>
-              <option value='vendor'>vendor</option>
-              <option value='ngo'>ngo</option>
-            </select>
-          </div>
-
-          <button
-            className='border bg-green-400 p-2 text-sm rounded-sm shadow-md text-white flex items-center gap-1'
-            onClick={handleSearch}
+      <div className='filters flex sm:flex-row flex-col mt-3 sm:max-w-5xl m-auto gap-2 w-[90%] justify-between'>
+        <div className='pl-2 flex gap-2 '>
+          <input
+            type='text'
+            placeholder='Brand Or SubBrand...'
+            className='text-xs  outline-none border p-2 w-[300px] rounded-sm'
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <select
+            className='border rounded-sm shadow-sm block  text-[12px] outline-none p-1'
+            onChange={(e) =>
+              setOrderType(
+                e.target.value === "All"
+                  ? undefined
+                  : VendorOrderType[
+                  e.target.value as keyof typeof VendorOrderType
+                  ]
+              )
+            }
           >
-            Filter <FaFilter />
-          </button>
+            {["All", ...Object.keys(VendorOrderType)].map((orderType) => (
+              <option value={orderType}>{orderType}</option>
+            ))}
+          </select>
+
+          <select
+            className='border rounded-md shadow-sm block text-[12px] outline-none p-1'
+            onChange={(e) =>
+              setOrderStatus(
+                e.target.value === "All"
+                  ? undefined
+                  : VendorOrderStatus[
+                  e.target.value as keyof typeof VendorOrderStatus
+                  ]
+              )
+            }
+          >
+            {["All", ...Object.keys(VendorOrderStatus)].map((status) => (
+              <option value={status} className=''>
+                {status}
+              </option>
+            ))}
+          </select>
+
+          <select
+            name='from'
+            className='border rounded-md shadow-md block w-full p-3 text-xs outline-none'
+          // value={from}
+          >
+            <option value='' disabled selected>From</option>
+            <option value='vendor'>vendor</option>
+            <option value='ngo'>ngo</option>
+          </select>
+
+          <select
+            name='to'
+            className='border rounded-md shadow-md block w-full p-3 text-xs outline-none'
+          // value={from}
+          >
+            <option value='' disabled selected>To</option>
+            <option value='vendor'>vendor</option>
+            <option value='ngo'>ngo</option>
+          </select>
+        </div>
+
+        <button
+          className='border bg-green-400 p-2 text-sm rounded-sm shadow-md text-white flex items-center gap-1'
+          onClick={handleSearch}
+        >
+          Filter <FaFilter />
+        </button>
+      </div>
+      <div className='table-container sm:max-w-5xl m-auto mt-4 w-[90%] shadow-lg rounded-md overflow-scroll sm:overflow-hidden overflow-y-hidden'>
+        <table className='p-4 w-full text-sm '>
+          <thead>
+            <tr className='border p-3 font-[400]'>
+              <th className='p-3 font-[600] border'>Index</th>
+              <th className='p-3 font-[600] border'>Brand</th>
+              <th className='p-3 font-[600] border'>subBrand</th>
+              <th className='p-3 font-[600] border'>From</th>
+              <th className='p-3 font-[600] border'>To</th>
+              <th className='p-3 font-[600] border'>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filterOrders.map((item, index: number) => {
+              console.log(item);
+              return (
+                <tr
+                  className={`border text-center text-xs ${index % 2 !== 0 ? "bg-gray-100" : ""
+                    } hover:bg-gray-200 cursor-pointer`}
+                  onClick={() => navigate(`/order-details/${item.id}`)}
+                >
+                  <td className='border p-2'>{index + 1}</td>
+                  <td className='border p-2'>{item.brand}</td>
+                  <td className='border p-2'>{item.subBrand}</td>
+                  <td className='border p-2'>{item.from}</td>
+                  <td className='border p-2'>{item.to}</td>
+                  <td className='border p-2 flex gap-2 items-center justify-center'>
+                    {item.status.length <= 0
+                      ? "Not Provided"
+                      : item.status[item.status.length - 1].status}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div className='table-container sm:max-w-5xl m-auto mt-4 w-[90%] shadow-lg rounded-md overflow-scroll sm:overflow-hidden overflow-y-hidden'>
-      <table className='p-4 w-full text-sm '>
-        <thead>
-          <tr className='border p-3 font-[400]'>
-            <th className='p-3 font-[600] border'>Index</th>
-            <th className='p-3 font-[600] border'>Brand</th>
-            <th className='p-3 font-[600] border'>subBrand</th>
-            <th className='p-3 font-[600] border'>From</th>
-            <th className='p-3 font-[600] border'>To</th>
-            <th className='p-3 font-[600] border'>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filterOrders.map((item, index: number) => {
-            console.log(item);
-            return (
-              <tr
-                className={`border text-center text-xs ${
-                  index % 2 !== 0 ? "bg-gray-100" : ""
-                } hover:bg-gray-200 cursor-pointer`}
-                onClick={() => navigate(`/order-details/${item.id}`)}
-              >
-                <td className='border p-2'>{index + 1}</td>
-                <td className='border p-2'>{item.brand}</td>
-                <td className='border p-2'>{item.subBrand}</td>
-                <td className='border p-2'>{item.from}</td>
-                <td className='border p-2'>{item.to}</td>
-                <td className='border p-2 flex gap-2 items-center justify-center'>
-                  {item.status.length <= 0
-                    ? "Not Provided"
-                    : item.status[item.status.length - 1].status}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  </div>
   );
 }
 
--- src/Pages/Order History/OrderHistory.tsx
+++ src/Pages/Order History/OrderHistory.tsx
@@ -1,17 +1,53 @@
 import Error from "../../Components/ErrorHandler/Error";
 import Loading from "../../Components/Loading/Loading";
 import OrderHistoryTable from "../../Components/OrderHistoryTable";
-
+import axiosInstance from '../../utils/axiosInstance';
+import { GET_ALL_VENDOR_ORDER } from '../../utils/restEndPoints';
+import { setLoading, setError } from '../../redux/slices/statusSlice';
+import { toast } from "react-toastify";
+import { Action } from '../../types/error';
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { VendorOrder } from "../../types/VendorOrder";
 
 const OrderHistory: React.FC = () => {
-  console.log("OrderHistory rendered");
+  const [orders, setOrders] = useState<VendorOrder[]>([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch(setLoading(true));
+        const response = await axiosInstance.get(GET_ALL_VENDOR_ORDER);
+        console.log(response.data)
+        setOrders(response.data);
+        //   setFilterOrders(response.data);
+      } catch (error: any) {
+        if (error.response) {
+          dispatch(
+            setError({
+              statusCode: error.response.status,
+              message: error.response.data.error,
+              action: Action.VENDOR_ORDER_HISTORY,
+            })
+          );
+        } else {
+          toast.error("Server is Down.");
+        }
+      } finally {
+        dispatch(setLoading(false));
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Error>
       <Loading>
-      <div>
-       <OrderHistoryTable/>
-      </div>
+        <div>
+          <OrderHistoryTable orders={orders}/>
+        </div>
       </Loading>
     </Error>
   );
