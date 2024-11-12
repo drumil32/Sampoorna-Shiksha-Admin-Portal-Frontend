306df9fe23d5242ebf469877a3448087f235448c
Drumil Akhenia <86913953+drumil32@users.noreply.github.com>
2024-11-12 22:46:29 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,22 @@ import App from './App.tsx'
 import { Provider } from 'react-redux';
 import store from './redux/store.ts';
 import './index.css'
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer position="bottom-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored" />
     </Provider >
   </StrictMode>
   ,
